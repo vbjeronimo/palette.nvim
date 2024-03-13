3781b0ad1ef2057d7b68c6b4074bf5e39d9d4263
Vitor Jeronimo <vitor.bently@hotmail.com>
2024-03-13 18:05:44 -0300

    Extracted config-related stuff from 'init.lua' into 'config.lua'

---
+++ lua/palette/config.lua
@@ -0,0 +1,31 @@
+local M = {}
+
+M.defaults = {
+  base_colors = {
+    range = {0, 15}
+  },
+  special_colors = {
+    foreground = {
+      group = "Normal",
+      attr = "fg"
+    },
+    background = {
+      group = "Normal",
+      attr = "bg"
+    },
+    cursor = {
+      group = "Cursor",
+      attr = "bg"
+    },
+  },
+  default_format = "json",
+  default_output_path = "~/palette.json",
+}
+
+M.options = {}
+
+M.set_options = function(opts)
+  M.options = vim.tbl_deep_extend("force", M.defaults, opts or {})
+end
+
+return M
--- lua/palette/init.lua
+++ lua/palette/init.lua
@@ -1,47 +1,9 @@
+local config = require("palette.config")
+
 local M = {}
 
-local defaults = {
-  base_colors = {
-    range = {0, 15}
-  },
-  special_colors = {
-    foreground = {
-      group = "Normal",
-      attr = "fg"
-    },
-    background = {
-      group = "Normal",
-      attr = "bg"
-    },
-    cursor = {
-      group = "Cursor",
-      attr = "bg"
-    },
-    -- TODO: delete these test configs
-    pmenukindsel = {
-      group = "PmenuKindSel",
-      attr = "fg"
-    },
-    pmenusel = {
-      group = "PmenuSel",
-      attr = "fg"
-    },
-    normalnc = {
-      group = "NormalNc",
-      attr = "bg"
-    },
-    -- test = {
-    --   group = "VertSplit",
-    --   attr = "bcg"
-    -- },
-    -- TODO END
-  },
-  default_format = "json",
-  default_output_path = "~/palette.json",
-}
-
 M.setup = function(opts)
-  M.config = vim.tbl_deep_extend("force", defaults, opts or {})
+  config.set_options(opts)
 end
 
 M.export_colorscheme = function()
