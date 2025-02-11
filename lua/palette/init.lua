35686a8c9392181c725062f35c88892bb407b330
Vitor Jeronimo <vitor.jeronimo@proton.me>
2025-02-10 21:18:05 -0400

    Added logic to handle multiple levels of linked hls

--- lua/palette/init.lua
+++ lua/palette/init.lua
@@ -24,15 +24,25 @@ M.setup = function(opts)
 end
 
 function get_special_color(group_name, attr_type)
+  local color_decimal = _get_color_decimal_value(group_name, attr_type)
+  return _decimal_to_hex(color_decimal)
+end
+
+function _get_color_decimal_value(group_name, attr_type)
   local highlight = vim.api.nvim_get_hl(GLOBAL_NAMESPACE_ID, { name = group_name })
 
   local color_decimal
-  if highlight[attr_type] then
+  if highlight["link"] then
+    color_decimal = _get_color_decimal_value(highlight["link"], attr_type)
+  elseif highlight[attr_type] then
     color_decimal = highlight[attr_type]
   end
 
-  local color_hex = string.lower(string.format("#%06x", color_decimal))
-  return color_hex
+  return color_decimal
+end
+
+function _decimal_to_hex(value)
+  return string.lower(string.format("#%06x", value))
 end
 
 M.get_special_color = get_special_color
